8718636c0aa6087b815c5716ad5f9f8cbc10bffc
Stefan Hein Bengtson <shbe@create.aau.dk>
2021-09-20 12:16:50 +0200

    updated readme and added encoder weights

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Pose  Estimation  from  RGB  Images  of  Highly  Symmetric  Objects using  a  Novel  Multi-View  Loss  and  Differential  Rendering
+## Pose  Estimation  from  RGB  Images  of  Highly  Symmetric  Objects using  a  Novel  Multi-Pose  Loss  and  Differential  Rendering
 
 ## Overview
 
@@ -10,35 +10,59 @@ Pending...
 
 ## Setup
 
-Install docker and some requirements to make the image build properly.
+1) Install docker and setup nvidia runtime environment.
+   - Install nvidia-docker2
+     (see https://gist.github.com/Brainiarc7/a8ab5f89494d053003454efc3be2d2ef)
 
- - Install nvidia-docker2
- (see https://gist.github.com/Brainiarc7/a8ab5f89494d053003454efc3be2d2ef)
+   - Add "nvidia" as default runtime to build Docker image with CUDA support. Required for pytorch3d. (see https://stackoverflow.com/questions/59691207/docker-build-with-nvidia-runtime)
 
-- Add "nvidia" as default runtime to build Docker image with CUDA support. Required for pytorch3d.
-(see https://stackoverflow.com/questions/59691207/docker-build-with-nvidia-runtime)
+2) Build docker image(s)
 
-- Clone this repo to a folder you will share with docker, default is that the repository is a subfolder to `~/share-to-docker`. You can instead change the share variables in `dockerfile/pytorch3d/run-gpu0.sh` to point to where you place the repo if required.
+  - Clone this repo:
+   ```
+   git clone git@github.com:shbe-aau/multi-pose-estimation.git ~/share-to-docker/multi-pose-estimation
+   ```
 
-- Run `bash build.sh` in `dockerfile/pytorch3d` to build the Docker image.
+
+   - Run `bash build.sh` in `multi-pose-esitmation/dockerfile/pytorch3d` to build the Docker images.
+   - (Optional) Run `bash build.sh` in `multi-pose-esitmation/dockerfile/aae` to build the Docker image for running the AAE (Augmented Autoencoder) - this is only needed for evaluation of the AAE approach.
 
 Tested working on Ubuntu 16.04 LTS with Docker 18.09.7 and NVIDIA docker 2.3.0, and on Ubuntu 16.04 LTS with Docker 19.03.4 and NVIDIA docker 2.3.0.
 
 ## Prepare models and data
 
-Get background images. We use the VOC2012 dataset as background images, they can be found at http://host.robots.ox.ac.uk/pascal/VOC/voc2012/ and default is to store them in the repo under `pytorch3d/data/VOC2012`.
+1) Download background images. We use the VOC2012 devkit for background images, they can be found at http://host.robots.ox.ac.uk/pascal/VOC/voc2012/#devkit Store them in the repo under `multi-pose/data/VOC2012/JPEGImages`.
 
-Get CAD files or use default as provided in `pytorch3d/data/cad-files`
+2) (Optional) Use the default for the T-LESS dataset already provided in `multi-pose/data/cad-files` or place your own CAD files in this folder.
 
-Get base encoder https://dlrmax.dlr.de/get/b42e7289-7558-5da0-8f26-4c472ad830a9/ as provided from https://github.com/DLR-RM/AugmentedAutoencoder/tree/multipath
+3) (Optional) Prepare the encoder weights. You have the following options:
 
-Instructions for training your own base encoder will be added later.
+  a. Use the default pretrained encoder weights already provided in 'multi-pose/data/encoder/obj1-18/encoder.npy'
+
+  b. Get the [pretrained encoder](https://dlrmax.dlr.de/get/b42e7289-7558-5da0-8f26-4c472ad830a9/) as provided from the [AAE repo](https://github.com/DLR-RM/AugmentedAutoencoder/tree/multipath).
+
+  c. Train your own autoencoder as specified in the [AAE repo](https://github.com/DLR-RM/AugmentedAutoencoder/tree/multipath).
+
+4) (Optional - only necessary for option __3b__ or __3c__) Convert the trained encoder from Tensorflow to Pytorch format.
+   - Instructions will be added later.
 
 ## Train
 
-Start the docker container with the script in `dockerfile/pytorch3d/run-gpu0.sh`.
+1) (Optional) Update the default path for the shared folder in `dockerfile/pytorch3d/run-gpu0.sh` - the shared folder should include the cloned repo. The default shared folder is `~/share-to-docker`. This step is only necessary if you did not clone the repo to the default location.
 
-To train a pose regression network on top of the pretrained encoder you can run `train.py experiment_template.cfg` in the folder `pytorch3d` inside the docker container. Parameters for the training are set in the .cfg file.
+2) Spin up the Docker container and start training:
+   ```
+   bash ~/share-to-docker/multi-pose-estimation/dockerfiles/pytorch3d/run-gpu0.sh
+   cd shared-folder/multi-pose-estimation/multi-pose
+   python train.py experiment_template.cfg
+   ```
+   Parameters for the training are set in the .cfg file.
+
+
+### Evaluating
+
+1) Bla
+
 
 ## Visualize loss landscape
 
---
+++ muti-pose/data/encoder/obj1-18/Encoder.py
@@ -0,0 +1,105 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+__weights_dict = dict()
+
+def load_weights(weight_file):
+    if weight_file == None:
+        return
+
+    try:
+        weights_dict = np.load(weight_file).item()
+    except:
+        weights_dict = np.load(weight_file, encoding='bytes').item()
+
+    return weights_dict
+
+class KitModel(nn.Module):
+
+
+    def __init__(self, weight_file):
+        super(KitModel, self).__init__()
+        global __weights_dict
+        __weights_dict = load_weights(weight_file, allow_pickle=True)
+
+        self.obj1_18_v2_conv2d_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d/Conv2D', in_channels=3, out_channels=128, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.obj1_18_v2_batch_normalization_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization/FusedBatchNorm', num_features=128, eps=0.0010000000474974513, momentum=0.0)
+        self.obj1_18_v2_conv2d_1_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d_1/Conv2D', in_channels=128, out_channels=256, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.obj1_18_v2_batch_normalization_1_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization_1/FusedBatchNorm', num_features=256, eps=0.0010000000474974513, momentum=0.0)
+        self.obj1_18_v2_conv2d_2_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d_2/Conv2D', in_channels=256, out_channels=512, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.obj1_18_v2_batch_normalization_2_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization_2/FusedBatchNorm', num_features=512, eps=0.0010000000474974513, momentum=0.0)
+        self.obj1_18_v2_conv2d_3_Conv2D = self.__conv(2, name='obj1_18_v2/conv2d_3/Conv2D', in_channels=512, out_channels=512, kernel_size=(5, 5), stride=(2, 2), groups=1, bias=True)
+        self.obj1_18_v2_batch_normalization_3_FusedBatchNorm = self.__batch_normalization(2, 'obj1_18_v2/batch_normalization_3/FusedBatchNorm', num_features=512, eps=0.0010000000474974513, momentum=0.0)
+        self.obj1_18_v2_dense_MatMul = self.__dense(name = 'obj1_18_v2/dense/MatMul', in_features = 32768, out_features = 128, bias = True)
+
+    def forward(self, x):
+        obj1_18_v2_Flatten_flatten_Reshape_shape_1 = torch.tensor(-1, dtype=torch.int32)
+        obj1_18_v2_conv2d_Conv2D_pad = F.pad(x, (1, 2, 1, 2))
+        obj1_18_v2_conv2d_Conv2D = self.obj1_18_v2_conv2d_Conv2D(obj1_18_v2_conv2d_Conv2D_pad)
+        obj1_18_v2_conv2d_Relu = F.relu(obj1_18_v2_conv2d_Conv2D)
+        obj1_18_v2_batch_normalization_FusedBatchNorm = self.obj1_18_v2_batch_normalization_FusedBatchNorm(obj1_18_v2_conv2d_Relu)
+        obj1_18_v2_conv2d_1_Conv2D_pad = F.pad(obj1_18_v2_batch_normalization_FusedBatchNorm, (1, 2, 1, 2))
+        obj1_18_v2_conv2d_1_Conv2D = self.obj1_18_v2_conv2d_1_Conv2D(obj1_18_v2_conv2d_1_Conv2D_pad)
+        obj1_18_v2_conv2d_1_Relu = F.relu(obj1_18_v2_conv2d_1_Conv2D)
+        obj1_18_v2_batch_normalization_1_FusedBatchNorm = self.obj1_18_v2_batch_normalization_1_FusedBatchNorm(obj1_18_v2_conv2d_1_Relu)
+        obj1_18_v2_conv2d_2_Conv2D_pad = F.pad(obj1_18_v2_batch_normalization_1_FusedBatchNorm, (1, 2, 1, 2))
+        obj1_18_v2_conv2d_2_Conv2D = self.obj1_18_v2_conv2d_2_Conv2D(obj1_18_v2_conv2d_2_Conv2D_pad)
+        obj1_18_v2_conv2d_2_Relu = F.relu(obj1_18_v2_conv2d_2_Conv2D)
+        obj1_18_v2_batch_normalization_2_FusedBatchNorm = self.obj1_18_v2_batch_normalization_2_FusedBatchNorm(obj1_18_v2_conv2d_2_Relu)
+        obj1_18_v2_conv2d_3_Conv2D_pad = F.pad(obj1_18_v2_batch_normalization_2_FusedBatchNorm, (1, 2, 1, 2))
+        obj1_18_v2_conv2d_3_Conv2D = self.obj1_18_v2_conv2d_3_Conv2D(obj1_18_v2_conv2d_3_Conv2D_pad)
+        obj1_18_v2_conv2d_3_Relu = F.relu(obj1_18_v2_conv2d_3_Conv2D)
+        obj1_18_v2_batch_normalization_3_FusedBatchNorm = self.obj1_18_v2_batch_normalization_3_FusedBatchNorm(obj1_18_v2_conv2d_3_Relu)
+        obj1_18_v2_Flatten_flatten_Shape = list(obj1_18_v2_batch_normalization_3_FusedBatchNorm.size())
+        #obj1_18_v2_Flatten_flatten_Reshape = torch.reshape(input = obj1_18_v2_batch_normalization_3_FusedBatchNorm, shape = (-1,32768))
+        intermediate = obj1_18_v2_batch_normalization_3_FusedBatchNorm.permute(0,2,3,1)
+        obj1_18_v2_Flatten_flatten_Reshape = torch.reshape(input = intermediate, shape = (-1,32768))
+
+        obj1_18_v2_Flatten_flatten_strided_slice = obj1_18_v2_Flatten_flatten_Shape[0:1]
+        obj1_18_v2_dense_MatMul = self.obj1_18_v2_dense_MatMul(obj1_18_v2_Flatten_flatten_Reshape)
+        obj1_18_v2_Flatten_flatten_Reshape_shape = [obj1_18_v2_Flatten_flatten_strided_slice,obj1_18_v2_Flatten_flatten_Reshape_shape_1]
+        return obj1_18_v2_dense_MatMul
+
+
+    @staticmethod
+    def __dense(name, **kwargs):
+        layer = nn.Linear(**kwargs)
+        layer.state_dict()['weight'].copy_(torch.from_numpy(__weights_dict[name]['weights']))
+        if 'bias' in __weights_dict[name]:
+            layer.state_dict()['bias'].copy_(torch.from_numpy(__weights_dict[name]['bias']))
+        return layer
+
+    @staticmethod
+    def __batch_normalization(dim, name, **kwargs):
+        if   dim == 1:  layer = nn.BatchNorm1d(**kwargs)
+        elif dim == 2:  layer = nn.BatchNorm2d(**kwargs)
+        elif dim == 3:  layer = nn.BatchNorm3d(**kwargs)
+        else:           raise NotImplementedError()
+
+        if 'scale' in __weights_dict[name]:
+            layer.state_dict()['weight'].copy_(torch.from_numpy(__weights_dict[name]['scale']))
+        else:
+            layer.weight.data.fill_(1)
+
+        if 'bias' in __weights_dict[name]:
+            layer.state_dict()['bias'].copy_(torch.from_numpy(__weights_dict[name]['bias']))
+        else:
+            layer.bias.data.fill_(0)
+
+        layer.state_dict()['running_mean'].copy_(torch.from_numpy(__weights_dict[name]['mean']))
+        layer.state_dict()['running_var'].copy_(torch.from_numpy(__weights_dict[name]['var']))
+        return layer
+
+    @staticmethod
+    def __conv(dim, name, **kwargs):
+        if   dim == 1:  layer = nn.Conv1d(**kwargs)
+        elif dim == 2:  layer = nn.Conv2d(**kwargs)
+        elif dim == 3:  layer = nn.Conv3d(**kwargs)
+        else:           raise NotImplementedError()
+
+        layer.state_dict()['weight'].copy_(torch.from_numpy(__weights_dict[name]['weights']))
+        if 'bias' in __weights_dict[name]:
+            layer.state_dict()['bias'].copy_(torch.from_numpy(__weights_dict[name]['bias']))
+        return layer
