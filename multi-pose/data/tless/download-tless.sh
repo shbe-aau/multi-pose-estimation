c11d20fa065c7bba03a266608a467a499a514917
Stefan Hein Bengtson <shbe@create.aau.dk>
2022-03-23 11:00:01 +0100

    download script now downloads the tless train dataset as well. added script for converting TLESS training data to validation pickle

--- multi-pose/data/tless/download-tless.sh
+++ multi-pose/data/tless/download-tless.sh
@@ -6,3 +6,7 @@ rm tless-models.zip
 wget "https://ptak.felk.cvut.cz/6DB/public/bop_datasets/tless_test_primesense_bop19.zip" --no-check-certificate -O tless-test-images.zip
 unzip tless-test-images.zip
 rm tless-test-images.zip
+
+wget "https://ptak.felk.cvut.cz/6DB/public/bop_datasets/tless_train_primesense.zip" --no-check-certificate -O tless-train-images.zip
+unzip tless-train-images.zip
+rm tless-train-images.zip
